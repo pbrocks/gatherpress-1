f49afa682f19bae63341de87afcd9fec5ab4ccc9
Paul Barthmaier <pbrocks@users.noreply.github.com>
2023-01-20 10:29:21 -0500

    build assets

--- build/blocks/venue/index.asset.php
+++ build/blocks/venue/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'bba35d1eb31401caf1f2');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'c22515b15192e22d3fec');
